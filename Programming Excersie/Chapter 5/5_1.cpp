360cf3d9d8c9f0fc0d0193435d7c97a2970d7dad
AbuBokkar95 <cyberpros.team@gmail.com>
2024-07-10 22:15:25 +0600

    Updated Chapter 5

--- Programming Excersie/Chapter 5/5_1.cpp
+++ Programming Excersie/Chapter 5/5_1.cpp
@@ -15,10 +15,79 @@
 
 Write a main program to test the program
 */
-
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-int main()
-{
+
+class BankAccount {
+private:
+    string name;
+    int accountNumber;
+    string accountType;
+    int balance;
+
+public:
+    void setValues(string name, int accountNumber, string accountType, int balance);
+    void deposit();
+    void withdraw();
+    void display() const;
+};
+
+void BankAccount::setValues(string name, int accountNumber, string accountType, int balance) {
+    this->name = name;
+    this->accountNumber = accountNumber;
+    this->accountType = accountType;
+    this->balance = balance;
+}
+
+void BankAccount::deposit() {
+    int amount;
+    cout << "Enter deposit amount: ";
+    cin >> amount;
+    balance += amount;
+    cout << "New balance: " << balance << endl;
+}
+
+void BankAccount::withdraw() {
+    int amount;
+    cout << "Enter withdraw amount: ";
+    cin >> amount;
+    if (amount > balance) {
+        cout << "Insufficient Balance." << endl;
+    } else {
+        balance -= amount;
+        cout << "New balance: " << balance << endl;
+    }
+}
+
+void BankAccount::display() const {
+    cout << "Name: " << name << endl
+         << "Account Number: " << accountNumber << endl
+         << "Account Type: " << accountType << endl
+         << "Balance: " << balance << endl;
+}
+
+int main() {
+    string name;
+    int accountNumber;
+    string accountType;
+    int balance = 0;
+
+    cout << "Enter name: ";
+    getline(cin, name);
+
+    cout << "Enter account number: ";
+    cin >> accountNumber;
+    cin.ignore();  
+
+    cout << "Enter account type: ";
+    getline(cin, accountType);
+
+    BankAccount account;
+    account.setValues(name, accountNumber, accountType, balance);
+    account.display();
+    account.deposit();
+    account.withdraw();
+    account.display();
     return 0;
 }
--- Programming Excersie/Chapter 5/5_2.cpp
+++ Programming Excersie/Chapter 5/5_2.cpp
@@ -1,5 +1,5 @@
 /*
-5.2: Write a class to represent a vector (a series of float values). Include member functions 
+5.2: Write a class to represent a vector (a series of float values). Include member functions
 to perform the following tasks:
     (a) To create the vector.
     (b) To modify the value of a given element.
@@ -8,9 +8,87 @@ to perform the following tasks:
 Write a program to test your class.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+class vector
+{
+private:
+    int element[100];
+    int size;
+
+public:
+    vector(); // Constructor to initialize size
+    void create(int size, int ele[]);
+    void modify(int index, int value);
+    void multiple(int scalar);
+    void display();
+};
+vector ::vector()
+{
+    size = 0;
+}
+void vector ::create(int size, int ele[])
+{
+    this->size = size;
+    for (int i = 0; i < size; i++)
+    {
+        this->element[i] = ele[i];
+    }
+}
+void vector ::modify(int index, int value)
+{
+    element[index] = value;
+}
+void vector ::multiple(int scalar)
+{
+    for (int i = 0; i < size; i++)
+    {
+        element[i] = element[i] * scalar;
+    }
+}
+void vector ::display()
+{
+    cout << "(";
+    for (int i = 0; i < size; i++)
+    {
+        cout << element[i];
+        if (i != size - 1)
+        {
+            cout << ", ";
+        }
+        else
+        {
+            cout << ")";
+        }
+    }
+    cout << endl;
+}
 int main()
 {
+    // int size = 5;
+    //  cout << "Enter the size of the vector: ";
+    //  cin >> size;
+
+    int element[5] = {10, 20, 30, 40, 50};
+
+    vector v1;
+    v1.create(5, element);
+    v1.display();
+
+    // int index, value;
+    // cout << "Enter index and value: ";
+    // cin >> index >> value;
+
+    v1.modify(2, 25);
+    v1.display();
+
+    // int scalar;
+    // cout << "Enter a number: ";
+    // cin >> scalar;
+
+    v1.multiple(2);
+    v1.display();
+
     return 0;
 }
